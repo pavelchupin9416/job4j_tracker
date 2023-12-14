40e4d41c60b0478f0d208feab1775a48ba57b861
PavelChupin <pavelchupin94@yandex.ru>
2023-12-14 15:44:06 +0300

    2. @Setter, @Getter и другие. Фикс.

--- src/main/java/ru/job4j/tracker/model/Category.java
+++ src/main/java/ru/job4j/tracker/model/Category.java
@@ -5,7 +5,6 @@ import lombok.*;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @RequiredArgsConstructor
 public class Category {
-    @NonNull
     @Getter
     @EqualsAndHashCode.Include
     private int id;
